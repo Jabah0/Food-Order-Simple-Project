21fd5ba6555a623f0a1bf628d0adbf55a35ddec1
jabah <mo.jabah0@gmail.com>
2023-02-18 21:29:57 +0200

    handel fetch meals error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,31 +5,49 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://jabah-d713b-default-rtdb.firebaseio.com/meals.json"
       );
+      if (!response.ok) {
+        throw Error("Something went wrong.");
+      }
       const data = await response.json();
       const loadedMeals = [];
 
-      for (const key in data){
+      for (const key in data) {
         loadedMeals.push({
           id: key,
           name: data[key].name,
           description: data[key].description,
           price: data[key].price,
-        })
+        });
       }
 
       setMeals(loadedMeals);
 
+      setIsLoading(false);
+
       console.log(loadedMeals);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return <p className={classes.loadding}>Loading...</p>;
+  }
+
+  if (httpError) {
+    return <p className={classes.error}>{httpError}</p>;
+  }
 
   const mealList = meals.map((meal) => (
     <MealItem
@@ -49,8 +67,3 @@ const AvailableMeals = () => {
 };
 
 export default AvailableMeals;
-
-
-
-
-
